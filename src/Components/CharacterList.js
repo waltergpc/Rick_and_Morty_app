4fae00a27e61ecb29efcdc5126591ec338003b03
Walter González-Plata Cortes <waltergpc@hotmail.com>
2022-02-15 09:58:33 -0600

    finish first styling for list view in characters page

--- src/Components/CharacterList.js
+++ src/Components/CharacterList.js
@@ -1,16 +1,56 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const CharacterList = ({ character }) => {
   const { name, image, species, status } = character
   return (
-    <article>
-      awefcasdfcasdfcasdfasdf
-      <img src={image} alt={name} />
-      <div>{name}</div>
-      <div>{species}</div>
-      <div>{status}</div>
-    </article>
+    <ArticleWrapper>
+      <img className='avatar' src={image} alt={name} />
+      <div className='list-info'>{name}</div>
+      <div className='list-info'>{species}</div>
+      <div className='list-info mobile-hidden'>{status}</div>
+    </ArticleWrapper>
   )
 }
 
 export default CharacterList
+
+const ArticleWrapper = styled.article`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  border: 1px solid black;
+  border-radius: 1rem;
+  padding: 0.5rem;
+  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+  background-color: rgba(47, 47, 46, 0.9);
+  color: beige;
+  align-items: center;
+
+  .avatar {
+    height: 3rem;
+    width: 3rem;
+    border-radius: 50%;
+  }
+
+  .list-info {
+    margin: 0.3rem;
+    font-size: 0.6rem;
+  }
+
+  .mobile-hidden {
+    display: none;
+  }
+
+  @media (min-width: 900px) {
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    padding: 1rem;
+
+    .list-info {
+      font-size: 1rem;
+    }
+
+    .mobile-hidden {
+      display: block;
+    }
+  }
+`
--- src/Components/ListView.js
+++ src/Components/ListView.js
@@ -1,16 +1,67 @@
 import React from 'react'
 import CharacterList from './CharacterList'
+import styled from 'styled-components'
 
 const ListView = ({ provider, data }) => {
   if (provider === 'characters') {
     return (
-      <div>
-        {data.map((character) => (
-          <CharacterList key={character.id} character={character} />
-        ))}
-      </div>
+      <Wrapper>
+        <div className='list-heading'>
+          <span>Avatar</span>
+          <span>Name</span>
+          <span>Species</span>
+          <span className='mobile-hidden'>Status</span>
+        </div>
+        <div className='list-body'>
+          {data.map((character) => (
+            <CharacterList key={character.id} character={character} />
+          ))}
+        </div>
+      </Wrapper>
     )
   }
 }
 
 export default ListView
+
+const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  background-color: rgba(47, 47, 46, 0.4);
+  border-radius: 0.5rem;
+  padding: 2rem;
+  gap: 1rem;
+  height: 55vh;
+  width: 85vw;
+  margin: auto;
+
+  .list-heading {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    border: 1px solid black;
+    border-radius: 1rem;
+    padding: 0.5rem;
+    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+    background-color: rgba(47, 47, 46, 0.9);
+    color: beige;
+  }
+
+  .list-body {
+    display: grid;
+    gap: 0.5rem;
+    overflow: scroll;
+  }
+
+  .mobile-hidden {
+    display: none;
+  }
+
+  @media (min-width: 900px) {
+    .list-heading {
+      grid-template-columns: 1fr 1fr 1fr 1fr;
+    }
+    .mobile-hidden {
+      display: block;
+    }
+  }
+`
